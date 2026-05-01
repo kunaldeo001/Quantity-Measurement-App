422bb356f14a4e34360393642f725592cfc4c44b
Kunal Deo <kunaldeo001@gmail.com>
2026-05-01 23:43:38 +0530

    Extend Unit enum with Volume and Weight support

--- src/main/java/com/quantitymeasurement/Unit.java
+++ src/main/java/com/quantitymeasurement/Unit.java
@@ -3,7 +3,18 @@ package com.quantitymeasurement;
 public enum Unit {
     INCH(1.0, MeasurementType.LENGTH),
     FEET(12.0, MeasurementType.LENGTH),
-    YARD(36.0, MeasurementType.LENGTH);
+    YARD(36.0, MeasurementType.LENGTH),
+    CM(0.4, MeasurementType.LENGTH),
+
+    // Volume units
+    GALLON(3.78, MeasurementType.VOLUME),
+    LITRE(1.0, MeasurementType.VOLUME),
+    ML(0.001, MeasurementType.VOLUME),
+
+    // Weight units
+    KILOGRAM(1.0, MeasurementType.WEIGHT),
+    GRAM(0.001, MeasurementType.WEIGHT),
+    TONNE(1000.0, MeasurementType.WEIGHT);
 
     public final double conversionFactor;
     public final MeasurementType type;
@@ -14,6 +25,6 @@ public enum Unit {
     }
 
     public enum MeasurementType {
-        LENGTH
+        LENGTH, VOLUME, WEIGHT
     }
 }
